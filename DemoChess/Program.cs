d352ee1e1693a97661e8c6cfcb8ffc4f0fc5c839
Nikita Tumanov <tumanovnikita999@gmail.com>
2022-04-07 20:21:13 +0600

    asd

---
+++ DemoChess/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess;
+
+namespace DemoChess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Chess.Chess chess = new Chess.Chess();
+            List<string> list;
+            while (true)
+            {
+                list = chess.GetAllMoves();
+                Console.WriteLine(chess.fen);
+                Print(ChessToAscii(chess));
+                Console.WriteLine(chess.IsChek() ? "CHECK" : "-");
+                foreach ( string moves in chess.GetAllMoves() )
+                {
+                    Console.Write(moves + "\t");
+                }
+                Console.WriteLine();
+                Console.Write(">");
+                string move = Console.ReadLine();               
+                if (move == "q")
+                {
+                    break;
+                }  
+                if(move == "") move = list [random.Next(list.Count)];
+                chess = chess.Move(move);
+            }
+        }
+        static string ChessToAscii (Chess.Chess chess)
+        {
+            string text = "  +-----------------+\n";
+            for (int y = 7;y >= 0; y--)
+            {
+                text += y + 1;
+                text += " | ";
+                for (int x = 0; x < 8 ; x++)
+                {
+                    text += chess.GetFigureAt(x, y) + " ";
+
+                }
+                text += "|\n";
+            }
+            text += "  +-----------------+\n";
+            text += "    a b c d e f g h\n";
+            return text;
+        }
+
+        static void Print(string text)
+        {
+            ConsoleColor oldForeColor = Console.ForegroundColor;
+            foreach (char x in text)
+            {
+                if (x >= 'a' && x <= 'z')
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (x >= 'A' && x <= 'Z')
+                    Console.ForegroundColor = ConsoleColor.White;
+                else 
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(x);
+            }
+            Console.ForegroundColor = oldForeColor;
+
+        }
+
+    }
+}
